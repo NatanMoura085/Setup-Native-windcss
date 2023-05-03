e4b448b1ca6961f6b9be715992944d2fd412c51f
Natan Moura <99056912+NatanMoura085@users.noreply.github.com>
2023-05-03 14:35:57 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-2xl text-black font-bold'>Hello Natan</Text>
+    </View>
+  );
+}
